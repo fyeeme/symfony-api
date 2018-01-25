53c684f1c2ab92b0cb2514ec3f811b5bcb27953d
Allen Lau <fyeeme@gmail.com>
2018-01-25 16:04:56 +0800

    init project with symfony 4.0.5

---
+++ README.md
@@ -0,0 +1,3 @@
+
+> 开发用的,可以删掉
+**  composer require server  
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+    public function index()
+    {
+        $number = mt_rand(0, 100);
+        return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+    }
+}
