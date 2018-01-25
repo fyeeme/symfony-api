72baf8c49fb6fd0339486f6e2605c9995e0e1360
Allen Lau <fyeeme@gmail.com>
2018-01-25 16:23:22 +0800

    add profiler bundle ,render response with twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,8 +3,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController
+class DefaultController extends Controller
 {
     /**
      * @Route("/luck")
@@ -12,6 +13,8 @@ class DefaultController
     public function index()
     {
         $number = mt_rand(0, 100);
-        return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+        return $this->render('luck/number.html.twig', [
+            'number'=>$number
+        ]);
     }
 }
