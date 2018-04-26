bdaf4c28a88c0bbd58fbe2c008c7e4ded0e2635c
Allen Lau <fyeeme@gmail.com>
2018-04-26 17:10:01 +0800

    validator demo

--- src/Controller/EndPointController.php
+++ src/Controller/EndPointController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Api\PathMeta;
+use Codeages\Biz\Framework\Context\Biz;
+use Codeages\Biz\Framework\Validation\SimpleValidator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,7 +36,6 @@ class EndPointController extends Controller
      */
     public function index(Request $request)
     {
-
         $response = $this->get('api.kernel')->handle($request);
 
         return new JsonResponse($response);
@@ -54,5 +55,28 @@ class EndPointController extends Controller
         );
     }
 
+    /**
+     * @return Biz
+     */
+    private function getKernel()
+    {
+        return $this->get('biz');
+    }
+
+
+    private function demo()
+    {
+        $this->getKernel()->service('UserService');
+        $user = [
+            'name' => '2d',
+            'password' => '3'
+        ];
+
+        $validator = new SimpleValidator();
+        $validator->validate($user, [
+            'name' => 'required|integer',
+            'password' => 'required|integer|length_between:2,10'
+        ]);
+    }
 
 }
