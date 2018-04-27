e267dd07adfec180d783e51f613208376d36af55
Allen Lau <fyeeme@gmail.com>
2018-04-27 14:14:24 +0800

    set default timezone

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
+//The default timezone set to 'Asia/Shanghai'
+date_default_timezone_set('Asia/Shanghai');
+
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
@@ -31,7 +34,6 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
-
 $kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
