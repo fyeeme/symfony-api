4b3b44cc86df781395cd1eb3d2d669a6563cd50d
Allen Lau <fyeeme@gmail.com>
2018-05-05 17:14:34 +0800

    code format

--- src/Controller/EndPointController.php
+++ src/Controller/EndPointController.php
@@ -2,25 +2,22 @@
 
 namespace App\Controller;
 
-use App\Api\PathMeta;
 use Codeages\Biz\Framework\Context\Biz;
 use Codeages\Biz\Framework\Validation\SimpleValidator;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 
 /**
- * Class EndPointController
- * @package App\Controller
+ * Class EndPointController.
+ *
  * @Route("/api")
  */
 class EndPointController extends Controller
 {
     /**
-     *
      * @Route("/")
      * @Route("/{res1}")
      * @Route("/{res1}/{slug1}")
@@ -32,15 +29,14 @@ class EndPointController extends Controller
      * @Route("/{res1}/{slug1}/{res2}/{slug2}/{res3}/{slug3}/{res4}/{slug4}")
      *
      * @param Request $request
+     *
      * @return Response
      */
     public function index(Request $request)
     {
-
         $response = $this->get('api.kernel')->handle($request);
 
         return new JsonResponse($response);
-
     }
 
     //----------------仅供测试，稍后会删掉-----------------------
@@ -57,29 +53,28 @@ class EndPointController extends Controller
         );
     }
 
-
     protected function testService()
     {
         $this->getKernel()->service('UserService');
         $user = [
             'name' => '2d',
-            'password' => '3'
+            'password' => '3',
         ];
 
         $validator = new SimpleValidator();
         $validator->validate($user, [
             'name' => 'required|integer',
-            'password' => 'required|integer|length_between:2,10'
+            'password' => 'required|integer|length_between:2,10',
         ]);
     }
 
-    protected function testRedis(){
+    protected function testRedis()
+    {
         $this->getKernel()->offsetGet('redis')->set('a', 2);
 
         $this->getRedis()->expire('a', 10);
         var_dump($this->getRedis()->get('a'));
     }
-
 
     /**
      * @return Biz
@@ -96,5 +91,4 @@ class EndPointController extends Controller
     {
         return $this->getKernel()->offsetGet('redis');
     }
-
 }
